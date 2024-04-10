174eed512258b3d484a743086cf638dcb572f418
Shiyang Zhang <harryzhang957@gmail.com>
2024-04-10 17:07:05 +0000

    finish the Expenses and HR_join base table

--- models/base/google_drive/base_google_drive_EXPENSES.sql
+++ models/base/google_drive/base_google_drive_EXPENSES.sql
@@ -5,8 +5,10 @@ SELECT
     _FIVETRAN_SYNCED, 
     CAST(DATE AS DATETIME) AS DATE_TIME, 
     EXPENSE_TYPE, 
-    CASE 
-        WHEN SUBSTR(EXPENSE_AMOUNT, 1, 1) = '$' THEN REPLACE(EXPENSE_AMOUNT, '$ ', '') 
-        ELSE EXPENSE_AMOUNT 
-    END AS EXPENSE_AMOUNT
+    CAST(
+            CASE 
+                WHEN SUBSTR(EXPENSE_AMOUNT, 1, 1) = '$' THEN REPLACE(EXPENSE_AMOUNT, '$ ', '') 
+                ELSE EXPENSE_AMOUNT 
+            END 
+        AS NUMERIC) AS EXPENSE_AMOUNT
 FROM {{ source('google_drive', 'EXPENSES') }}
