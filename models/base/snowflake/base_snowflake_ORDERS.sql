e35253a6a56321dee59fdc398d00825de8357476
shiyang Zhang <harryzhang957@gmail.com>
2024-04-23 23:51:28 +0000

    finish changing

--- models/base/snowflake/base_snowflake_ORDERS.sql
+++ models/base/snowflake/base_snowflake_ORDERS.sql
@@ -1,17 +1,54 @@
-select
+-- select
+--     _FIVETRAN_ID,
+--     STATE,
+--     SESSION_ID,
+--     CAST(REPLACE(SHIPPING_COST, 'USD ', '') AS NUMERIC) AS SHIPPING_COST,
+--     PAYMENT_METHOD,
+--     PHONE,
+--     TAX_RATE,
+--     CLIENT_NAME,
+--     ORDER_AT AS ORDER_AT_TS,
+--     ORDER_ID,
+--     SHIPPING_ADDRESS,
+--     PAYMENT_INFO,
+--     _FIVETRAN_DELETED,
+--     _FIVETRAN_SYNCED AS _FIVETRAN_SYNCED_T
+
+-- FROM {{ source('Snowflake', 'ORDERS') }}
+
+WITH RankedSessions AS (
+    SELECT
+        _FIVETRAN_ID,
+        STATE,
+        SESSION_ID,
+        CAST(REPLACE(SHIPPING_COST, 'USD ', '') AS NUMERIC) AS SHIPPING_COST,
+        PAYMENT_METHOD,
+        PHONE,
+        TAX_RATE,
+        CLIENT_NAME,
+        ORDER_AT AS ORDER_AT_TS,
+        ORDER_ID,
+        SHIPPING_ADDRESS,
+        PAYMENT_INFO,
+        _FIVETRAN_DELETED,
+        _FIVETRAN_SYNCED AS _FIVETRAN_SYNCED_T,
+        ROW_NUMBER() OVER (PARTITION BY ORDER_ID ORDER BY SESSION_ID DESC) AS ro -- Adjust ORDER BY if a different column defines "latest"
+    FROM {{ source('Snowflake', 'ORDERS') }}
+)
+SELECT
     _FIVETRAN_ID,
     STATE,
     SESSION_ID,
-    CAST(REPLACE(SHIPPING_COST, 'USD ', '') AS NUMERIC) AS SHIPPING_COST,
+    SHIPPING_COST,
     PAYMENT_METHOD,
     PHONE,
     TAX_RATE,
     CLIENT_NAME,
-    ORDER_AT AS ORDER_AT_TS,
+    ORDER_AT_TS,
     ORDER_ID,
     SHIPPING_ADDRESS,
     PAYMENT_INFO,
     _FIVETRAN_DELETED,
-    _FIVETRAN_SYNCED AS _FIVETRAN_SYNCED_T
-
-FROM {{ source('Snowflake', 'ORDERS') }}
+    _FIVETRAN_SYNCED_T
+FROM RankedSessions
+WHERE ro = 1
